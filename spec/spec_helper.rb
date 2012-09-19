d1ce8463a0a682b87eee40d56c7399b6b054dc5b
Romain THERRAT <romain42@gmail.com>
2012-09-19 12:03:58 +0200

    Fix test ... ugly but working ...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 # Based on https://github.com/puppetlabs/puppetlabs-ntp/blob/master/spec/spec_helper.rb
-# Thanks to Ken Barber for advice about http://projects.puppetlabs.com/issues/11191  
+# Thanks to Ken Barber for advice about http://projects.puppetlabs.com/issues/11191
 require 'puppet'
 require 'rspec-puppet'
 require 'tmpdir'
@@ -16,7 +16,7 @@ RSpec.configure do |c|
   end
 
   c.after :each do
-    FileUtils.remove_entry_secure(@puppetdir)
+    #FileUtils.remove_entry_secure(@puppetdir)
   end
 
   c.module_path = File.join(File.dirname(__FILE__), '../../')
